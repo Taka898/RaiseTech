30a94b197079acd89b653ab336526af6594217b2
Takashi MORI <taka@TakashinoMacBook-Pro.local>
2023-09-05 13:38:48 +0900

    serverspecにテストを追加

--- serverspec/spec/cfn-lecture-ec2/sample_spec.rb
+++ serverspec/spec/cfn-lecture-ec2/sample_spec.rb
@@ -1,6 +1,31 @@
 require 'spec_helper'
 
-listen_port = 8080
+listen_port = 80
+
+describe command('ruby -v') do
+  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+  its(:stdout) { should match /ruby 3.1.2/ }
+end
+
+describe package('bundler') do
+  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+  it { should be_installed.by('gem').with_version('2.3.14') }
+end
+
+describe package('rails') do
+  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+  it { should be_installed.by('gem').with_version('7.0.4') }
+end
+
+describe command('node -v') do
+  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('yarn -v') do
+  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+  its(:exit_status) { should eq 0 }
+end
 
 describe package('nginx') do
   it { should be_installed }
@@ -11,8 +36,13 @@ describe service('nginx') do
   it { should be_running }
 end
 
+describe package('unicorn') do
+  let(:path) { '/home/ec2-user/.rbenv/shims:$PATH' }
+  it { should be_installed.by('gem') }
+end
+
 # （追加テスト）Nginxの設定ファイルが存在することを確認
-describe file('/etc/nginx/conf.d/localhost.conf') do
+describe file('/etc/nginx/conf.d/rails.conf') do
   it { should be_file }
 end
 
@@ -20,6 +50,6 @@ describe port(listen_port) do
   it { should be_listening }
 end
 
-describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
+describe command('curl http://127.0.0.1:#{listen_port}/ -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
 end
